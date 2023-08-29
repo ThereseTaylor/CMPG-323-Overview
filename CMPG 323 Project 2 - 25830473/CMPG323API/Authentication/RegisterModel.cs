26649359c0bfab3e978014ca7695a8c6b102947a
ThereseTaylor <112098539+ThereseTaylor@users.noreply.github.com>
2023-08-29 15:36:40 +0200

    Add Authenticate Controller and Update Program.cs

--- CMPG 323 Project 2 - 25830473/CMPG323API/Authentication/RegisterModel.cs
+++ CMPG 323 Project 2 - 25830473/CMPG323API/Authentication/RegisterModel.cs
@@ -7,7 +7,6 @@ namespace JWTAuthentication.Authentication
         [Required(ErrorMessage = "User Name is required")]
         public string? Username { get; set; }
 
-
         [EmailAddress]
         [Required(ErrorMessage = "Email is required")]
         public string? Email { get; set; }
