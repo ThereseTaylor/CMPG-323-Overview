99431fa9114eec5a56d30341903c4145be7930ba
ThereseTaylor <112098539+ThereseTaylor@users.noreply.github.com>
2023-09-14 16:58:28 +0200

    Add Repos and Populate CustomerRepo

---
+++ CMPG 323 Project 3 - 25830473/SuperStore P3/SuperStore P3/Repository/CustomerRepository.cs
@@ -0,0 +1,54 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace EcoPower_Logistics.Repository
+{
+    public class CustomerRepository
+    {
+        protected readonly SuperStoreContext _context = new SuperStoreContext();
+
+        // GET ALL: Customers
+        public IEnumerable<Customer> GetAll()
+        {
+            return _context.Customers.ToList();
+        }
+
+        // TO DO: Add ‘Get By Id’
+        public Customer GetCustomerByID(int id)
+        {
+            return _context.Customers.Find(id);
+        }
+
+        // TO DO: Add ‘Create’
+        public void InsertCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+        }
+
+        // TO DO: Add ‘Edit’
+        public void UpdateCustomer(Customer customer)
+        {
+            _context.Entry(customer).State = EntityState.Modified;
+
+        }
+
+        // TO DO: Add ‘Delete’
+        public void DeleteCustomer(int customerID)
+        {
+            Customer customer = _context.Customers.Find(customerID);
+            _context.Customers.Remove(customer);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        // TO DO: Add ‘Exists’
+        public bool Exists(int customerID)
+        {
+            return (_context.Customers?.Any(e => e.CustomerId == customerID)).GetValueOrDefault();
+        }
+    }
+}
